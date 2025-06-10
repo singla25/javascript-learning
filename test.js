7263ddef19a1a1e75bc0b4c95ffcee6ee3abc3e0
Sahil Singla <sahilrajiv.singla@gmail.com>
2025-06-10 09:29:45 +0530

    first commit

---
+++ test.js
@@ -0,0 +1,12 @@
+console.log("Welcome Sahil"); 
+
+let name = "Sahil";
+let age = 23;
+console.log(`Hi, my name is ${name} and my age is ${age}`);
+
+let username = prompt("Enter username: ");
+if(username === "Sahil") {
+    alert("Welcome back, Sahil!");
+} else {
+    alert("Access Denied");
+}
