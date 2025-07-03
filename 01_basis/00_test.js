36b07ed6d46041f0c841145631fd9f86c2120ebe
Sahil Singla <98223428+singla25@users.noreply.github.com>
2025-07-03 09:18:35 +0530

    commit 0

---
+++ 01_basis/00_test.js
@@ -0,0 +1,13 @@
+console.log("Welcome Sahil"); 
+console.log("Welcome to the world of JavaScript");
+
+let name = "Sahil";
+let age = 23;
+console.log(`Hi, my name is ${name} and my age is ${age}`);
+
+// let username = prompt("Enter username: ");
+// if(username === "Sahil") {
+//     alert("Welcome back, Sahil!");
+// } else {
+//     alert("Access Denied");
+// }
---
+++ 01_basis/01_variables.js
@@ -0,0 +1,26 @@
+// variables
+// constant
+
+const accountId = 12345
+let accountEmail = "sahil@sahil.com"
+var accountPassword = "98765"
+accountCity = "Chandigarh" // it is also a varible but not prefer
+let accountState
+
+// accountId = 1234 - it is not possible because we already define accountId as a contant
+console.log(accountId);
+
+accountEmail = "rajiv@rajiv.com"
+console.log(accountEmail);
+
+accountPassword = "9876543210"
+console.log(accountPassword);
+
+accountCity = "TriCity"
+console.log(accountCity);
+
+accountState
+console.log(accountState);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+ 
