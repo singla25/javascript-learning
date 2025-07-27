58601d0593bd6b1d105c4de4d5606e7d26aa1040
Sahil Singla <98223428+singla25@users.noreply.github.com>
2025-07-27 18:51:21 +0530

    jsExecution

---
+++ 03_basics/05_executionContext.js
@@ -0,0 +1,46 @@
+// There are 3 main types of execution contexts:
+// -Global Execution Context (GEC)
+// -Function Execution Context (FEC)
+// -Eval Execution Context (very rare; from eval())
+
+
+// How JavaScript Executes Code (Phases)
+// -Global Phase
+// -Creation Phase(Memory Phase)
+// -Execution Phase 
+
+// Try this in snippet in google chrome to see visual relization
+function one() {
+    console.log('one')
+}
+
+function two() {
+    console.log('two')
+}
+
+function three() {
+    console.log('three')
+}
+
+one()
+two()
+three()
+
+
+function one() {
+    console.log('one')
+    two()
+}
+
+function two() {
+    console.log('two')
+    three()
+}
+
+function three() {
+    console.log('three')
+}
+
+one()
+two()
+three()
