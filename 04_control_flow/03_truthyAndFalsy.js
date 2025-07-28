8276479cd17daf89d5f00f8ac2565fb751d4ce15
Sahil Singla <98223428+singla25@users.noreply.github.com>
2025-07-28 09:54:26 +0530

    truthy

---
+++ 04_control_flow/03_truthyAndFalsy.js
@@ -0,0 +1,40 @@
+const userEmail = 'sahil@gmail.com' //string == true value
+if (userEmail) {
+    console.log('Got USer Email')
+} else {
+    console.log("Don't have user email")
+}
+console.log()
+
+const email = ''  //empty == false
+if (email) {
+    console.log('Got USer Email')
+} else {
+    console.log("Don't have user email")
+}
+console.log()
+
+const array = []  //empty == true
+if (array) {
+    console.log('Got USer Email')
+} else {
+    console.log("Don't have user email")
+}
+console.log()
+
+// falsy values - false, 0, -0, BigInt On, "", null, undefined
+// truthy values - "0", 'false', " ", [], {}, function(){}, all except falsy values
+
+if (array.length === 0) {
+    console.log("Array is empty")
+}
+console.log()
+
+const object = {}
+if (Object.keys(object).length  === 0) {
+    console.log('Object is empty')
+}
+
+// false == 0 true
+// false == '' true
+// 0 == '' true
