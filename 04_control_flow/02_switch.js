5cd9c7a1047fa98a20585d8d8a68d649d9c7a306
Sahil Singla <98223428+singla25@users.noreply.github.com>
2025-07-28 09:38:16 +0530

    switch

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,52 @@
+// Switch
+//  switch (expression) {
+//   case value1:
+//     // code block
+//     break;
+//   case value2:
+//     // code block
+//     break;
+//   default:
+//     // code block
+// }
+
+const month = 3
+switch (month) {
+    case 1:
+        console.log("January")
+        break
+    case 2:
+        console.log("February")
+        break
+    case 3:
+        console.log("March")
+        break
+    case 4:
+        console.log("April")
+        break
+    case 5:
+        console.log("May")
+        break
+    case 6:
+        console.log("June")
+        break
+    default:
+        console.log("Default case match")  
+}
+console.log()
+
+
+let fruit = "apple";
+switch (fruit) {
+  case "apple":
+    console.log("It’s an apple");
+    break;
+  case "banana":
+    console.log("It’s a banana");
+    break;
+  default:
+    console.log("Unknown fruit");
+}
+console.log()
+
+
