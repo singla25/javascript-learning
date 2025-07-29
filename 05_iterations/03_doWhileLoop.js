b67b6a9df58ddefc1f183489b0acb94549591364
Sahil Singla <98223428+singla25@users.noreply.github.com>
2025-07-29 13:08:11 +0530

    do while

---
+++ 05_iterations/03_doWhileLoop.js
@@ -0,0 +1,27 @@
+// do while loop
+let i = 1;
+do {
+  console.log(i);
+  i++;
+} while (i <= 5);
+console.log()
+
+let num = 10;
+do {
+  console.log("Runs at least once!");
+} while (num < 5);
+console.log()
+
+let score = 1
+do {
+    console.log(`Score is ${score}`);
+    score++;
+} while (score <= 10)
+console.log()
+
+let score1 = 11
+do {
+    console.log(`Score1 is ${score}`);
+    score1++;
+} while (score1 <= 10)
+console.log()
