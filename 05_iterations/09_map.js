8253ed0770a9267f139428874f6d2845024b3d04
Sahil Singla <98223428+singla25@users.noreply.github.com>
2025-07-30 23:54:37 +0530

    map functions

---
+++ 05_iterations/09_map.js
@@ -0,0 +1,29 @@
+// map - higher order functions
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const evenNums = myNums.map( (num) => num%2 == 0)
+console.log(evenNums);
+console.log()
+
+const oddNums = myNums.map( (num) => (num%2 != 0))
+console.log(oddNums);
+console.log()
+
+let newNums = myNums.map((num) => { return num+2 })
+console.log(newNums);
+console.log()
+
+// channing
+const myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+let newArray = myArray
+            .map((num) => num * 10)
+            .map((num) => num - 5)
+console.log(newArray)
+console.log()
+
+newArray = myArray
+            .map((num) => num * 10)
+            .map((num) => num - 5)
+            .filter( (num) => num >= 40)
+console.log(newArray)
