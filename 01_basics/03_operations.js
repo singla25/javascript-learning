3464c06e397a0d9ce5afcc7c7e7283f178548613
Sahil Singla <98223428+singla25@users.noreply.github.com>
2025-08-11 19:31:20 +0530

    revision

--- 01_basics/03_operations.js
+++ 01_basics/03_operations.js
@@ -32,7 +32,7 @@ console.log();
 
 // some problems with string concatenation
 console.log("Concatenation with number '5' + 2 :", '5' + 2); // "52"
-console.log("Concatenation with number 5 + '2' :", 5 + '5'); // "52"
+console.log("Concatenation with number 5 + '2' :", 5 + '2'); // "52"
 console.log("Concatenation with number '5' + 2 + 2 :", '5' + 2 + 2); // "522"
 console.log("Concatenation with number 5 + '2' + 2 :", 5 + '2' + 2); // "522"
 console.log("Concatenation with number 5 + 2 + '2' :", 5 + 2 + '2'); // "72"
@@ -65,10 +65,12 @@ console.log("5 < 10:", 5 < 10); // true
 console.log("5 > 10:", 5 > 10); // false
 console.log("5 <= 10:", 5 <= 10); // true   
 console.log("5 >= 10:", 5 >= 10); // false
+console.log()
 
 // some problems with comparison operations
 console.log("'2' > 1 : ", '2' > 1); 
 console.log("'02' > 1 : ", '02' > 1); 
+console.log()
 
 console.log(" null > 0 : ", null > 0);
 console.log(" null < 0 : ", null < 0);
@@ -80,6 +82,7 @@ console.log(" null <= 0 : ", null <= 0);
 // the reason is that an equality check == and comparisons > < >= <= work differently.
 // Comparisonds convert null to a number, treating it as 0.
 // that's why null >= 0 is true and null > 0 is false
+console.log()
 
 console.log(" undefined == 0 : ", undefined == 0);
 console.log(" undefined > 0 : ", undefined > 0);
