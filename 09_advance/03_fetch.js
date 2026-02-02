ac6d42e9e0bdfc4384b87fe81f76fd3af877a45b
Sahil <sahil.ashriya@gmail.com>
2026-02-02 19:18:34 +0530

    fetch

--- 09_advance/03_fetch.js
+++ 09_advance/03_fetch.js
@@ -25,4 +25,32 @@ fetch("https://jsonplaceholder.typicode.com/users")
 .catch((error)=> console.log(error))
 
 
+fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+        title: "Hello",
+        body: "This is post body",
+        userId: 1
+    })
+})
+.then(res => res.json())
+.then(data => console.log(data))
+.catch(err => console.log(err));
+
+
+// fetch 404 / 500 pe reject nahi hota
+// response.ok check karna padta hai
+fetch("https://jsonplaceholder.typicode.com/posts")
+.then(response => {
+    if (!response.ok) {
+        throw new Error("HTTP Error");
+    }
+    return response.json();
+})
+.then(data => console.log(data))
+.catch(err => console.log(err));
+
 
